59c531a0a22f3c2850b3f9c4dff3823d4b4ccf38
vallak-bitch <70606325+vallak-bitch@users.noreply.github.com>
2021-01-30 03:19:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://accurate-functional-mallet.glitch.me"]
+const urls = ["https://glitch.com/edit/#!/accurate-functional-mallet"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
